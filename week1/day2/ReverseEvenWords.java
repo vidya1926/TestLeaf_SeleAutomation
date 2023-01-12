a58f566da85d9deb9eb224ff12f004c1314951b7
Karthickperiasamy <39276985+Karthickperiasamy@users.noreply.github.com>
2023-01-12 19:00:43 +0530

    Update ReverseEvenWords.java

--- week1/day2/ReverseEvenWords.java
+++ week1/day2/ReverseEvenWords.java
@@ -7,27 +7,30 @@ public class ReverseEvenWords {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// Need to Check
-		
-  		String test = "I am a software tester";
-  		String even = "";
-  		String rev = "";
-  		String[] split = test.split(" ");
-  		for (int i = 1; i < split.length; i++) {
-  			if(i%2 != 0) {
-  			even = even + split[i];}
-  						
-		}
+String input="I am a software tester";//3 0 1 2 olleh
+String[] split = input.split(" ");
 
-		for (int j = even.length()-1; j >=0; j--) {
-			rev = rev+ even.charAt(j);
-				
-		}
-		for (int k = 0; k < split.length; k++) {
-			if (k%2==0) {System.out.print(split[k]+" ");}
-			
-		}
-		
+for (int i = 0; i < split.length; i++) {
+    //System.out.println(split[i]);
+    if(i%2==0) {
+        
+        System.out.print(split[i]);
+        
+        System.out.print(" ");
+    }
+    else {
+        
+        char[] charArray = split[i].toCharArray();
+        
+        for (int j =charArray .length-1; j>=0 ; j--) {
+            
+            System.out.print(charArray[j]);
+            
+        }
+        System.out.print(" ");
+    }
+        
+    
+    }
 	}
-		
 }
