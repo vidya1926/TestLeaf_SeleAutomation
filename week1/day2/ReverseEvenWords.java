a11714663a8d07809d425dc69cb227b190c59b37
vidya1926 <83778108+vidya1926@users.noreply.github.com>
2023-01-12 18:18:26 +0530

    Update ReverseEvenWords.java

--- week1/day2/ReverseEvenWords.java
+++ week1/day2/ReverseEvenWords.java
@@ -31,3 +31,22 @@ public class ReverseEvenWords {
 	}
 		
 }
+
+String test = "I am a software tester";
+  		String rev = "";
+  		String[] split = test.split(" ");
+  		for (int i = 0; i < split.length; i++) {
+  			if(i%2 == 0) {
+  				char[] charArray = split[i].toCharArray();
+  				for (int j = charArray.length-1; j>=0; j--) {
+  					System.out.print(charArray[j]);
+									}
+  				System.err.print( " ");
+  				
+  			}else {
+  				System.out.print(split[i]);
+  				
+  			}
+  			
+  			}
+  			}
